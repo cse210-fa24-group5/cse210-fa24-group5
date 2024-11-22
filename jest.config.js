b208df8e62148d3e75e82d4ba6669dbb4f105e58
Jakob Getzel <jakobgetzel@gmail.com>
2024-11-22 13:26:01 -0800

    added eslint prettier integration + formatted files with prettier

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,25 @@
-
 /** @type {import('jest').Config} */
 const config = {
-    coverageThreshold: {
-      global: {
-        branches: 80,
-        functions: 80,
-        lines: 80,
-        statements: 80,
-      },
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
-    projects: [
-      {
-        'displayName': 'browser',
-        'testEnvironment': 'jest-environment-jsdom',
-        'testMatch': ['**/tests/unit/**'],
-      },
-      {
-        "displayName": "node",
-        "testEnvironment": "node",
-        "testMatch": ["**/tests/integration/**"]
-      }
-    ],
-  };
-  
-  module.exports = config;
+  },
+  projects: [
+    {
+      'displayName': 'browser',
+      'testEnvironment': 'jest-environment-jsdom',
+      'testMatch': ['**/tests/unit/**'],
+    },
+    {
+      "displayName": "node",
+      "testEnvironment": "node",
+      "testMatch": ["**/tests/integration/**"]
+    }
+  ],
+};
+
+module.exports = config;
--- tests/unit/sum.test.js
+++ tests/unit/sum.test.js
@@ -1,7 +1,6 @@
-const sum = require('../../src/sum');
+const sum = require("../../src/sum");
+const { test, expect } = require("@jest/globals");
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(
-    sum(1, 2)
-  ).toBe(3);
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
 });
