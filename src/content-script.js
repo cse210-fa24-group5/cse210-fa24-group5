e682dcca8b1775406a191771b0bc2e3637fb15b3
ekko-sde <jud010@ucsd.edu>
2024-11-25 12:27:06 +0800

    Update content-script.js

--- src/content-script.js
+++ src/content-script.js
@@ -1,15 +1,16 @@
 const observer = new MutationObserver((mutations) => {
     mutations.forEach(() => {
-      // 查找提交成功的提示元素
+   
       const successMessage = document.querySelector('span.text-green-s.dark\\:text-dark-green-s.text-sm.font-medium');
       if (successMessage && successMessage.textContent.includes('Accepted')) {
-        // 提取题目标题
+    
         const titleElement = document.querySelector('a.no-underline[href^="/problems/"]');
         if (titleElement) {
-          const problemTitle = titleElement.textContent.trim(); // 提取标题
+          let problemTitle = titleElement.textContent.trim();
+          problemTitle = problemTitle.replace(/^\d+\.\s/, '');
           console.log(`Submission successful for: ${problemTitle}`);
   
-          // 发送消息到扩展的弹窗或后台脚本
+  
           chrome.runtime.sendMessage({
             action: 'submissionSuccess',
             problemTitle,
@@ -19,7 +20,6 @@ const observer = new MutationObserver((mutations) => {
     });
   });
   
-  // 开始观察 DOM 变化
   observer.observe(document.body, { childList: true, subtree: true });
   
   console.log("Content script for LeetCode loaded!");
