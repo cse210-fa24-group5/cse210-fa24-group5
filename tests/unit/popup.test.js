4adf56616b6e864a2423f66d6c500ae64ebb3332
Jakob Getzel <jakobgetzel@gmail.com>
2024-11-27 15:34:58 -0800

    Update integration test to catch title

--- tests/unit/popup.test.js
+++
@@ -1,9 +0,0 @@
-// const { test, expect } = require("@jest/globals");
-
-// test("check 3 * 10 works", () => {
-//   expect(testFunction(3)).toBe(30);
-// });
-//
-// test("check -3 * 10 works", () => {
-//   expect(testFunction(-3)).toBe(-30);
-// });
