25217623c193303d7380d9be148279153ab1f95d
JackLiX0105 <jacklix0105@gmail.com>
2024-12-06 08:51:39 -0800

    further bug fix for dependency unit test

--- tests/unit/timer.test.js
+++ tests/unit/timer.test.js
@@ -185,6 +185,6 @@ describe("checkDifficulty Functionality", () => {
     expect(countdownElement.textContent).toBe("60:00"); // Hard should be 60:00 (60 minutes)
   });
   it("Detects environment correctly", () => {
-    expect(isESModuleSupported).toBe(true, "The environment should support ES modules");
+    expect(isESModuleSupported()).toBe(true, "The environment should support ES modules");
   });
 });
